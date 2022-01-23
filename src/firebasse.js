6adb6e1e8a54c63727ac4e4ea835dde1dd92de0f
Saurav singh <sauravsingh85079@gmail.com>
2022-01-23 23:07:53 +0530

    Add Basic Css and js

---
+++ src/firebasse.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBNIWCqCQyFvYCsHLtjKyZblt0tHyq2IJ8",
+    authDomain: "whatapp-reactjs.firebaseapp.com",
+    projectId: "whatapp-reactjs",
+    storageBucket: "whatapp-reactjs.appspot.com",
+    messagingSenderId: "347114601106",
+    appId: "1:347114601106:web:0dbc3c25ddcce73de9844b",
+    measurementId: "G-T2YY3QC4WY"
+  };
