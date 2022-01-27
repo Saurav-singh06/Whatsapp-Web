6442ac0c853530a0006378f2b5e270a079b3e16e
Saurav singh <sauravsingh85079@gmail.com>
2022-01-27 22:48:21 +0530

    Add Chat Component

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,38 +1,57 @@
-import { AttachFile, MoreVert, Search } from '@mui/icons-material';
-import { Avatar, IconButton } from '@mui/material';
-import React, { useState , useEffect } from 'react';
-import "../Chat.css"
+import { AttachFile, InsertEmoticon, Message, Mic, MoreVert, Search, Send } from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import "../Chat.css";
 
 function Chat() {
-
   const [Seed, setSeed] = useState("");
 
   useEffect(() => {
- setSeed(Math.floor(Math.random()*5000));
+    setSeed(Math.floor(Math.random() * 5000));
   }, []);
-  
-  
-  return <div className='chat'>
-    <div className="chat_header">
-    <Avatar src={`https://avatars.dicebear.com/api/avataaars/${Seed}.svg`}/>
-    <div className="chat_headerInfo">
-      <h4>Room Name</h4>
-      <p>Last Seen at ...</p>
-    </div>
-    <div className="chat_headerRight">
-      <IconButton><Search/></IconButton>
-      <IconButton><AttachFile/></IconButton>
-<IconButton><MoreVert/></IconButton>
-    </div>
-    </div>
-    <div className="chat_body">
 
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar
+          src={`https://avatars.dicebear.com/api/avataaars/${Seed}.svg`}
+        />
+        <div className="chat_headerInfo">
+          <h4>Room Name</h4>
+          <p>Last Seen at ...</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <Search />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">
+        <p className={`chat_message ${true &&'chat_reciever'}`}>
+        <span className="chat_name">saurav</span>
+        Hey Guys
+        <span className="chat_timestamp">3:54pm</span>
+        </p>
+     
+        
+      </div>
+      <div className="chat_footer">
+        <IconButton><InsertEmoticon/></IconButton>
+        <form action="">
+        <input type="text" placeholder="Type a message" />
+        {/* <button>Send a Message</button> */}
+        <IconButton><Mic/></IconButton>
+        <IconButton className="sendIcon"><Send/></IconButton>
+        </form>
+      </div>
     </div>
-    <div className="chat_footer">
-
-    </div>
-  </div>;
+  );
 }
 
 export default Chat;
-
